156f68c0dfbd6272ac63d7ac9b2a040599d84ae8
anr <naveenreddyaleti@gmail.com>
2016-08-31 21:25:27 +0530

    added new sauce test case

---
+++ naveen/alti/SampleSauceTest.java
@@ -0,0 +1,34 @@
+package alti;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+ 
+import java.net.URL;
+ 
+public class SampleSauceTest {
+ 
+  public static final String USERNAME = "anreddy";
+  public static final String ACCESS_KEY = "1158b707-7baf-4b61-9628-fef5ebddf12d";
+  public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+ 
+  public static void main(String[] args) throws Exception {
+ 
+    //DesiredCapabilities caps = DesiredCapabilities.chrome();
+    DesiredCapabilities caps = DesiredCapabilities.firefox();
+    caps.setCapability("platform", "Windows XP");
+    caps.setCapability("version", "44.0");
+    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+ 
+    /**
+     * Goes to Sauce Lab's guinea-pig page and prints title
+     */
+ 
+   // driver.get("https://saucelabs.com/test/guinea-pig");
+    driver.get("https://facebook.com/");
+    System.out.println("title of page is: " + driver.getTitle());
+ 
+    driver.quit();
+  }
+}
+
+
